a93baa50d7cb8d2cbb87a2a688da16013476a878
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-04-23 15:40:46 -0300

    Criação do componente shopping card

--- meat-app/src/app/restaurant-detail/shopping-card/shopping-card.component.ts
+++ meat-app/src/app/restaurant-detail/shopping-card/shopping-card.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-card.service';
 
 @Component({
   selector: 'mt-shopping-card',
   templateUrl: './shopping-card.component.html'
 })
-export class ShoppingCardComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
   }
 
+  items(): any[] {
+    return this.shoppingCartService.items
+  }
+
+  total(): number {
+    return this.shoppingCartService.total()
+  }
+
+  clear() {
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item)
+    //console.log('ShoppingComponente')
+  }
 }
