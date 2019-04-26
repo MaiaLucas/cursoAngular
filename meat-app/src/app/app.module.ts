a390982e29bd3495b0c248486e31539a72af07a6
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-04-26 11:39:42 -0300

    Rota para o componente radio

--- meat-app/src/app/app.module.ts
+++ meat-app/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-card/shopping-card.service';
 import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/src/app/shared/radio/radio.component';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { InputComponent } from './shared/input/input.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
