58a6232cad6b2f949b6acb63468e6eac2663444d
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-04-30 13:55:31 -0300

    Implementação do checkOrder

--- meat-app/src/app/order/order.service.ts
+++ meat-app/src/app/order/order.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-card/shopping-card.service";
 import { CartItem } from "app/restaurant-detail/shopping-card/cart-item.model";
+import { Observable } from "rxjs/Observable";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import "rxjs/operator/map";
+import { Order } from "./order.model";
+import { MEAT_API } from "app/app.api";
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService) {}   
+    constructor(private cartService: ShoppingCartService, private http: Http) {}   
 
     cartItems(): CartItem[] {
         return this.cartService.items
@@ -25,6 +30,20 @@ export class OrderService {
 
     remove(item: CartItem) {
         this.cartService.removeItem(item)
+    }
+
+    clear() {
+        this.cartService.clear()
+    }
+
+    checkOrder(order:Order): Observable<string> {
+
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
+                        .map(response=> response.json())
+                        .map(order=> order.id)
+                        
     }
 
 }
