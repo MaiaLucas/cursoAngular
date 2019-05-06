bd3f7095b15f1f965818bd0121f3248ca5023676
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-05-06 14:44:46 -0300

    Tirando as declarações dos componentes order, orderItem e deliveryCosts

--- meat-app/src/app/app.module.ts
+++ meat-app/src/app/app.module.ts
@@ -9,7 +9,6 @@ import {ROUTES} from './app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { OrderComponent } from './order/order.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantService } from './restaurants/restaurant.service';
@@ -19,9 +18,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-card/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-card/shopping-card.service';
-import { OrderItemComponent } from './order/order-item/order-item.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -37,9 +34,6 @@ import { SharedModule } from './shared/shared.module';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    OrderItemComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent,
   ],
   imports: [
