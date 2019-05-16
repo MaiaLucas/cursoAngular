ee9ea99eeeb3b10652685f661399c5b5592caed6
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-05-16 11:54:14 -0300

    dist

--- meat-app/backend/dist/server.js
+++ meat-app/backend/dist/server.js
@@ -3,6 +3,8 @@ exports.__esModule = true;
 var jsonServer = require("json-server");
 var fs = require("fs");
 var https = require("https");
+var auth_1 = require("./auth");
+var authz_1 = require("./authz");
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
@@ -11,16 +13,15 @@ server.use(middlewares);
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
-server.post('/login', function (req, resp) {
-    resp.json({ message: 'ok' });
-});
+server.post('/login', auth_1.handleAuthentication);
+server.use('/orders', authz_1.handleAuthorization);
 // Use default router
 server.use(router);
 var options = {
     cert: fs.readFileSync('./backend/keys/cert.pem'),
     key: fs.readFileSync('./backend/keys/key.pem')
 };
-var port = 3001;
+var port = 3002;
 https.createServer(options, server).listen(port, function () {
     console.log("JSON Server is running on https://localhost:" + port);
 });
